fffc5f28b7c0de066576fe4af66c8a3e4044aaae
srinivasVlabs <srinivas.vykuntapu@valuelabs.com>
2020-11-01 23:03:04 +0530

    material table component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MyTableModule } from './components/table/table-module';
 import { TableExampleComponent } from './examples/table-example/table-example.component';
+import { ChildComponent } from './examples/child/child.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TableExampleComponent
+    TableExampleComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
-    MyTableModule
+    MyTableModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/examples/child/customer.ts
@@ -0,0 +1,4 @@
+export class Customer {
+    address: string;
+    name: string;
+  }
